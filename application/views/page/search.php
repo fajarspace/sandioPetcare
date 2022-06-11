e425678db3c481204bbea7d38505925765c5cfce
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-06-11 15:58:11 +0700

    rebuilding site Sat Jun 11 15:58:11 WIB 2022

--- application/views/page/search.php
+++ application/views/page/search.php
@@ -1,3 +1,4 @@
+<main class="container">
 <div class="wrapper">
     <div class="title-head">
             <h2 class="title">Hasil Pencarian: <?= $q ?> <?= $titleHead ?></h2>
@@ -34,18 +35,18 @@
             </div>
         </div>
         <?php $setting = $this->db->get('settings')->row_array(); ?>
-        <main class="container">
+
         <div class="produk-list">
             <?php if($products->num_rows() > 0){ ?>
                 <?php foreach($products->result_array() as $p): ?>
             <div class="produk-box">
                 <a href="<?= base_url(); ?>p/<?= $p['slug']; ?>">
                 <div>
-                  <img src="<?= base_url(); ?>assets/images/product/<?= $p['img']; ?>" > 
+                  <img src="<?= base_url(); ?>assets/images/product/<?= $p['img']; ?>" >
                     <div class="ket">
                       <h2 ><?= $p['title']; ?></h2>
                       <h3>Rp <?= str_replace(",",".",number_format($p['price'])); ?></h3>
-                        
+
                     </div>
                 </div>
                 </a>
@@ -54,10 +55,10 @@
         </div>
 
         </div>
-        </main>
+
             <div class="clearfix"></div>
             <?php }else{ ?>
                 <div class="alert alert-warning">Upss. Tidak ada produk yang dapat ditampilkan</div>
             <?php } ?>
     </div>
-</div>
+</main>
